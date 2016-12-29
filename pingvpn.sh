a974e0ce1ae7c6e8c778bbf16fdd8d9fb9ebdf37
rem@archbox <crimsonsky.rem@gmail.com>
2016-12-29 14:30:22 +0800

    change bash to zsh switch folder to NetworkManager

--- pingvpn.sh
+++ pingvpn.sh
@@ -1,19 +1,35 @@
-#!/bin/bash
-echo 'This is for testing speed of given openvpn files'
-DefaultFolder=$HOME/openvpn/Open_TCP
-while getopts ":f" optname
-do
-    case "$optname" in
-        "f")
-            DefaultFolder=$HOME/openvpn/Open_TCP/fast
-            ;;
-    esac
-done
-for file_a in ${DefaultFolder}/*; do
-    if [[ -d $file_a ]];then
-        break;
-    fi
-    tmp=$(echo $file_a|awk -F / '{print $NF}')
-    echo -n $tmp:
-    ping -c 4 $(awk '/remote/{print $2}' ${file_a})|tail -n 1|awk -F / '{print " "$5" ms"}'
-done
+#!/bin/zsh
+echo 'vpn script is running'
+active=$(nmcli con show --active |grep vpn|awk '{print $1}')
+if [ -z $active ]; then
+    DefaultFolder=/etc/NetworkManager/system-connections
+    speed=()
+    files=()
+    i=0
+    for file_a in ${DefaultFolder}/*; do
+        if [[ -d $file_a ]];then
+            break;
+        fi
+        tmp=$(echo $file_a|awk -F / '{print $NF}')
+        echo -n $tmp:
+        speed[$i]=$(ping -c 4 $(sudo awk 'BEGIN{FS="="}/remote/{print $2}' ${file_a} |awk 'BEGIN{FS=":"}{print $1}')|tail -n 1|awk -F / '{print $5}'|awk -F . '{print $1}')
+        files[$i]=$(echo ${file_a}|awk -F / '{print $5}')
+        echo -n ${speed[$i]}"ms | "
+        ((i++))
+    done
+    echo ""
+    min=${speed[0]}
+    pos=0
+    index=$[${#speed[*]}-1]
+    for j in `seq 1 $index`; do
+        if [ $min -gt ${speed[$j]} ]; then
+            min=${speed[$j]}
+            pos=$j
+        fi
+    done
+    echo "starting connection to "${files[$pos]}
+    nmcli con up id ${files[$pos]}
+else
+    echo "vpn connection already exist! connection id is "$active
+fi
+
