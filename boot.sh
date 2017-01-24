7cace93e081b1dbf5c0971529b7369dbe76b002e
rem@archbox <crimsonsky.rem@gmail.com>
2017-01-24 14:12:33 +0800

    deprecated keychain

--- boot.sh
+++ boot.sh
@@ -4,8 +4,9 @@ VBoxClient-all
 
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
-    ssh-add ~/.ssh/id_rsa
 fi
 if [[ "$SSH_AGENT_PID" == "" ]]; then
     eval "$(<~/.ssh-agent-thing)"
 fi
+
+ssh-add ~/.ssh/id_rsa
