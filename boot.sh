446a080bc6ee0d56496f28cd6b2060fb23367b1f
rem@archbox <crimsonsky.rem@gmail.com>
2017-01-24 14:03:44 +0800

    --amend

--- boot.sh
+++ boot.sh
@@ -2,4 +2,10 @@ sudo mount -t vboxsf VM_Connected ~/VM_Connected
 
 VBoxClient-all
 
-keychain -Q -q --nogui ~/.ssh/id_rsa
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh-agent-thing
+    ssh-add ~/.ssh/id_rsa
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+    eval "$(<~/.ssh-agent-thing)"
+fi
